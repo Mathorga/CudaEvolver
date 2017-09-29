9b23311598984734afe02637ae661818d1df233a
Luka Micheletti <michelettiluka@gmail.com>
2017-09-29 11:18:27 +0200

    Removed GeneticLibrary

--- main.cu
+++ main.cu
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Chromosome.h"
 
 __global__ void print() {
     printf("Hello, I am thread %d of block %d\n", threadIdx.x, blockIdx.x);
 }
 
 int main(int argc, char const *argv[]) {
-
+    GaChromosome dna = new GaChromosome();
     // Create the initial population.
 
     // Calculate the fitness of the population.
