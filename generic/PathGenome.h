5687ca9d538f501a1306b6ea916298c028a27b92
Luka Micheletti <michelettiluka@gmail.com>
2017-11-28 15:37:21 +0100

    Added license to source files

--- generic/PathGenome.h
+++ generic/PathGenome.h
@@ -1,3 +1,26 @@
+/*
+**************************************************************************
+PathGenome.h
+
+This file implements an implementation to the Genome class for the
+TSP.
+Copyright (C) 2017  Luka Micheletti
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+**************************************************************************
+*/
+
 #ifndef __PATH_GENOME__
 #define __PATH_GENOME__
 
